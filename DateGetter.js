9e15c41dfe81e8ffe4121a629425e75965560bf7
unknown <amir.yalamov@gmail.com>
2017-09-29 16:54:48 -0400

    Misc debugging

--- DateGetter.js
+++ DateGetter.js
@@ -1,7 +1,6 @@
 //Date4U js file, gets published date of webpage from source code of webpage
 //makes use of "datePublished": function
 
-function getDate() {
-  var element = document.getElementById("datePublished");
-  var date = element.innerText.search(/"datePublished":"(.+)"/)
-}
+var element = document.getElementById("datePublished");
+var date = element.innerText.search(/"datePublished":"(.+)"/)
+return date;
